a855124b2a0511e36882bd81c92fd57ef27aba9d
Walker <kyle.walker@tcu.edu>
2018-05-14 18:28:56 -0500

    commit place identity lecture slides

--- R/get-nba-data.R
+++ R/get-nba-data.R
@@ -1,8 +1,22 @@
 library(nbastatR)
 library(tidyverse)
 
-wolves <- get_teams_seasons_shots(teams = "Minnesota Timberwolves")
+teams <- get_nba_teams() %>%
+  filter(idConference != 0, 
+         !nameTeam %in% c("team Stephen", "TEAM Lebron"))
 
-tj <- filter(wolves, namePlayer == "Tyus Jones")
+nba <- get_teams_seasons_shots(teams = teams$nameTeam)
 
-write_csv(tj, "tyus_jones.csv")
+teamlist <- gsub("Los Angeles Clippers", "LA Clippers", teams$nameTeam)
+
+walk(teamlist, function(x) {
+  
+  team <- filter(nba, nameTeam == x)
+  
+  out <- sprintf("data/nba/%s.csv", x)
+  
+  write_csv(team, out)
+})
+
+
+
